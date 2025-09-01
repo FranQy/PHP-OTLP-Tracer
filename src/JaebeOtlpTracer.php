68e15f28f2af9117aa97767d4d25049d5e8b23c9
Przemysław Szychowski <przemyslaw.szychowski@AllesAuto-Mcbook-Pro-M2.local>
2025-09-01 11:01:13 +0200

    init repo

---
+++ src/JaebeOtlpTracer.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jaebe\OtlpTracer;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+
+class JaebeOtlpTracer extends AbstractBundle
+{
+    /**
+     * @param array<string, mixed> $config
+     */
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import(__DIR__ . '/../config/services.yaml');
+    }
+}
